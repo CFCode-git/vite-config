9ca7a9b5dab8ce8942a0fa9a3798c8c71554a6b3
周啟尧 <435184766@qq.com>
2022-04-07 22:20:08 +0800

    config: css module && postcss 配置 autoprefixer 插件.

---
+++ src/components/Comp2/index.tsx
@@ -0,0 +1,16 @@
+/*
+ * @Author: 周啟尧 zhouqy50@chinaunicom.cn
+ * @Date: 2022-04-07 18:11:04
+ * @LastEditTime: 2022-04-07 22:03:36
+ * @LastEditors: 周啟尧
+ * @Description: file content
+ */
+
+import style from './index.module.scss'
+
+export function Comp2(){
+  return (
+    <p className={style.comp2}>This is Comp2 -- css module</p>
+  )
+}
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: 周啟尧 zhouqy50@chinaunicom.cn
  * @Date: 2022-04-07 17:19:27
- * @LastEditTime: 2022-04-07 18:08:51
+ * @LastEditTime: 2022-04-07 22:16:43
  * @LastEditors: 周啟尧
  * @Description: file content
  */
@@ -9,16 +9,28 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 const variablePath = resolve("./src/variable.scss")
+const autoprefixer = require('autoprefixer') 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css:{
+    modules:{
+      generateScopedName: '[name]__[local]__[hash:base64:5]'
+    },
     preprocessorOptions: {
       scss:{
         // 注入全局 scss
         additionalData: `@import "${variablePath}";`
       }
+    },
+    postcss:{
+      plugins:[
+        autoprefixer({
+          // 指定目标浏览器
+          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11']
+        })
+      ]
     }
   }
 })
