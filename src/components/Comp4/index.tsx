e01ad1d57626ba5b80f4359723f3a4b3358f8e81
周啟尧 <435184766@qq.com>
2022-04-07 23:01:33 +0800

    config: <续> emotion 配置, tsconfig 添加 jsxImportSource, 避免 css 样式报错.

--- src/components/Comp4/index.tsx
+++ src/components/Comp4/index.tsx
@@ -1,16 +1,18 @@
 /*
  * @Author: 周啟尧 zhouqy50@chinaunicom.cn
  * @Date: 2022-04-07 22:38:36
- * @LastEditTime: 2022-04-07 22:55:55
+ * @LastEditTime: 2022-04-08 07:44:47
  * @LastEditors: 周啟尧
  * @Description: file content
  */
 
-/** @jsxImportSource @emotion/react */
 import { jsx,css } from '@emotion/react';
 
 export function Comp4() {
   return (
-    <div css={{color:'yellow'}}>hello</div>
+    <div css={{
+      color:'green',
+      fontSize: '1.5em'
+    }}> This is Comp4 -- emotion </div>
   );
 }
---
+++ windi.config.ts
@@ -0,0 +1,16 @@
+/*
+ * @Author: 周啟尧 zhouqy50@chinaunicom.cn
+ * @Date: 2022-04-08 07:48:54
+ * @LastEditTime: 2022-04-08 08:32:04
+ * @LastEditors: 周啟尧
+ * @Description: file content
+ */
+
+import {defineConfig} from 'vite-plugin-windicss'
+
+export default defineConfig({
+  attributify:true,
+  shortcuts: {
+    "flex-c": "flex justify-center items-center",
+  }
+})
